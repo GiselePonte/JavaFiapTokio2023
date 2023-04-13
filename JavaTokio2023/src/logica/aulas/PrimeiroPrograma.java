5ea7c39335ba16f0ae3c5dfb02174ee04b2cd2ff
GiselePonte <gisele.pontes19@gmail.com>
2023-04-13 18:25:02 -0300

    primeiro commit java logica

---
+++ JavaTokio2023/src/logica/aulas/PrimeiroPrograma.java
@@ -0,0 +1,33 @@
+package logica.aulas;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello, world!!  S2 ");
+		
+		
+int idade;
+idade=23;
+
+//sysout//
+System.out.println(idade);
+
+idade=17;
+System.out.println(idade);
+
+
+
+double salario= 1318.20;
+System.out.println(salario);
+
+String nome= "Gisele";
+System.out.println(nome);
+
+System.out.println("Seja bem vindo"+ nome+ "!");
+System.out.println(nome+ ", voce tem " + idade + " de idade.");
+
+	}
+
+}
+ 
---
+++ JavaTokio2023/src/logica/aulas/SegundoPrograma.java
@@ -0,0 +1,23 @@
+package logica.aulas;
+
+import java.util.Scanner;
+
+public class SegundoPrograma {
+
+	public static void main(String[] args) {
+
+Scanner sc= new Scanner(System.in);
+
+
+System.out.println("Digite seu nome: ");
+String nome= sc.nextLine();
+
+System.out.println("Bem vinda, " + nome );
+
+System.out.println("Digite sua idade: ");
+System.out.println("Sua idade é: "+ sc.nextLine());
+
+
+	}
+
+}
